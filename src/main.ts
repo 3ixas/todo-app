6e5c7f9f0103d1d242c7c292037f3bc24c16e391
Elias Bennett <eliasthebennett@gmail.com>
2025-01-29 13:43:43 +0000

    Adding the first part of the TS logic to my main.ts file. Setting variables to select the DOM elements & adding in a line that gets the todos from local storage so they remain after the page reloads.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
 import "../styles/main.scss";
+
+// Selecting the DOM elements
+const todoForm = document.getElementById("todo-form") as HTMLFormElement;
+const todoInput = document.getElementById("todo-input") as HTMLInputElement;
+const todoList = document.getElementById("todo-list") as HTMLUListElement;
+
+interface Todo {
+    id: number;
+    text: string;
+    completed: boolean;
+}
+
+// This gets the todos from local storage so they persist after the page reloads
+let todos: Todo[] = JSON.parse(localStorage.getItem("todos") || "[]");
